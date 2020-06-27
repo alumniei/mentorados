6b78489f93d120d3200e69ea8e7a8efd5b5cc0dc
Hugo Peixoto <hugo.peixoto@gmail.com>
2020-06-27 14:04:25 +0100

    Fix #current_user conflict with @user
    current_user used an instance variable @user. This conflicted
    with controllers that used the same variable.
    
    I fixed it by changing it to @current_user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,9 +12,9 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user
-    return @user if defined?(@user)
+    return @current_user if defined?(@current_user)
 
-    @user = User.find_by(id: session[:user_id])
+    @current_user = User.find_by(id: session[:user_id])
   end
 
   def bad_request
